d66f37d0d7b0e325646b958bb229bafa937766ae
zyzhaomingjie <757749718@qq.com>
2020-08-19 13:27:48 +0800

    Kmeans

---
+++ 数据挖掘基础/Kmeans_iris.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import  seaborn as sns 
+
+df=pd.DataFrame(datasets.load_iris()['data'],columns=datasets.load_iris()['feature_names']) 
+
+iris = datasets.load_iris() 
+X = iris.data[:, :4] ##表示我们取特征空间中的4个维度
+
+kmeans = KMeans(n_clusters=3).fit(X)
+
+kmeans.cluster_centers_###类均值
+
+
+y = kmeans.predict(X)
+
+sns.scatterplot(x=X[:,0],y=X[:,2],hue=y)
